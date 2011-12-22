a87fc389bbb9090e2d48e5451fb021d6155b69e3
Andrew Whitaker <aawhitaker@gmail.com>
2011-12-22 14:24:06 -0500

    Adding resultFn parameter, which allows a final transformation if needed.

--- src/jquery.aggregate.js
+++ src/jquery.aggregate.js
@@ -1,17 +1,21 @@
 (function($) {
-    // aggregate(object, [seed], fn
-    $.aggregate = function(object, seed, fn) {
+    $.aggregate = function(object, seed, fn, resultFn) {
         var val;
-        
-        if (typeof(fn) === 'undefined') {
-            fn = seed;
-        } else {
-            val = seed;
-        }
+		
+		if (typeof(seed) === "function") {
+			resultFn = fn;
+			fn = seed;
+		} else {
+			val = seed;
+		}
         
         $.each(object, function(index, element) {
             val = fn(val, element);
         });
+		
+		if (typeof(resultFn) === 'function') {
+			val = resultFn(val);
+		}
         
         return val;
     };
--- test/aggregate.js
+++ test/aggregate.js
@@ -7,12 +7,26 @@ test("Basic Tests", function () {
 });
 
 test("aggregate", function () {
-	expect(2);
+	expect(4);
 	
 	var fn = function (working, el) {
 		return working ? el + working : el;
 	};
 	
+	/* Without seed, with function */
 	equal(jQuery.aggregate([1, 2, 3], fn), 6);
-	equal(jQuery.aggregate("hello", fn), "olleh");
+	
+	/* With seed, without function */
+	equal(jQuery.aggregate([1, 2, 3], 10, fn), 16);
+	
+	var resultFn = function(val) {
+		return val + 10;
+	};
+	
+	/* Without seed, result function */
+	equal(jQuery.aggregate([1, 2, 3], fn, resultFn), 16);
+	
+	/* With seed, result function */
+	equal(jQuery.aggregate([1, 2, 3], 10, fn, resultFn), 26);
+
 });
