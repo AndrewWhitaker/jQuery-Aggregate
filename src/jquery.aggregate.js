e127e81e2cb31e2d2370516fbf3d2ab845b56116
Andrew Whitaker <aawhitaker@gmail.com>
2013-02-05 21:13:24 -0500

    Linting jquery.aggregate.js

--- src/jquery.aggregate.js
+++ src/jquery.aggregate.js
@@ -1,41 +1,43 @@
-(function($) {
+/*jslint unparam: true */
+/*global jQuery */
+(function ($) {
+    'use strict';
     $.aggregate = function (object, seed, fn, resultFn) {
         var val;
-        
-        if (typeof(seed) === "function") {
+
+        if (typeof (seed) === 'function') {
             resultFn = fn;
             fn = seed;
         } else {
             val = seed;
         }
-        
-        $.each(object, function(index, element) {
+
+        $.each(object, function (index, element) {
             val = fn.call(element, val, element);
         });
-        
-        if (typeof(resultFn) === 'function') {
+
+        if (typeof (resultFn) === 'function') {
             val = resultFn(val);
         }
-        
+
         return val;
     };
-    
+
     $.sum = function (object, fn) {
-        if (typeof(fn) === "undefined") {
+        if (fn === undefined) {
             fn = function (el) { return el; };
         }
-        
+
         return $.aggregate(object, function (val, element) {
             return val ? val + fn.call(this, element) : fn.call(this, element);
         });
     };
-    
+
     $.fn.aggregate = function (seed, fn, resultFn) {
         return $.aggregate(this, seed, fn, resultFn);
     };
-    
+
     $.fn.sum = function (fn) {
         return $.sum(this, fn);
     };
-    
-})(jQuery);
+}(jQuery));
